370fa961ccd9ab60db52a7b052de8ac9341f3f6d
Anka <runner@Mac-1745202060150.local>
2025-04-21 02:33:29 +0000

    KMP SPM package release for 0.1.6

--- Package.swift
+++ Package.swift
@@ -2,23 +2,20 @@
 import PackageDescription
 
 // BEGIN KMMBRIDGE VARIABLES BLOCK FOR 'core' (do not edit)
-let remotecoreUrl =
-    "https://api.github.com/repos/Lavmee/KMMBridge-Test/releases/assets/248055341.zip"
-let remotecoreChecksum = "d9d0df846194d3da63e65cb7d21deca406728d6431b4dc1322f97f3d1b012435"
+let remotecoreUrl = "https://api.github.com/repos/Lavmee/KMMBridge-Test/releases/assets/248069459.zip"
+let remotecoreChecksum = "870944eeda9f611ed9fc716ee30490a4017dfd389f67ba0907b90c7c699e0a27"
 let corePackageName = "core"
 // END KMMBRIDGE BLOCK FOR 'core'
 
 // BEGIN KMMBRIDGE VARIABLES BLOCK FOR 'auth' (do not edit)
-let remoteauthUrl =
-    "https://api.github.com/repos/Lavmee/KMMBridge-Test/releases/assets/248055341.zip"
-let remoteauthChecksum = "d9d0df846194d3da63e65cb7d21deca406728d6431b4dc1322f97f3d1b012435"
+let remoteauthUrl = "https://api.github.com/repos/Lavmee/KMMBridge-Test/releases/assets/248069341.zip"
+let remoteauthChecksum = "a8481ce7cfbe60fbbab2f7ac9def0b981da76c39b9f657b8a7336f3720b41bb5"
 let authPackageName = "auth"
 // END KMMBRIDGE BLOCK FOR 'auth'
 
 // BEGIN KMMBRIDGE VARIABLES BLOCK FOR 'user' (do not edit)
-let remoteuserUrl =
-    "https://api.github.com/repos/Lavmee/KMMBridge-Test/releases/assets/248055341.zip"
-let remoteuserChecksum = "d9d0df846194d3da63e65cb7d21deca406728d6431b4dc1322f97f3d1b012435"
+let remoteuserUrl = "https://api.github.com/repos/Lavmee/KMMBridge-Test/releases/assets/248069521.zip"
+let remoteuserChecksum = "3d88f061aa6164770bb32c342a152fba6c0ea53d21103d72a376b518b580ab06"
 let userPackageName = "user"
 // END KMMBRIDGE BLOCK FOR 'user'
 
