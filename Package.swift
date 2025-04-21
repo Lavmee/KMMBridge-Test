cc6e20af5f602eb204c7e34821af8e25804fd4fc
Anka <runner@Mac-1745198534445.local>
2025-04-21 01:33:35 +0000

    KMP SPM package release for 0.1.1

--- Package.swift
+++ Package.swift
@@ -1,47 +1,29 @@
 // swift-tools-version:5.8
 import PackageDescription
 
-let corePackageName = "core"
-let authPackageName = "auth"
+// BEGIN KMMBRIDGE VARIABLES BLOCK FOR 'user' (do not edit)
+let remoteuserUrl = "https://api.github.com/repos/Lavmee/KMMBridge-Test/releases/assets/248055341.zip"
+let remoteuserChecksum = "d9d0df846194d3da63e65cb7d21deca406728d6431b4dc1322f97f3d1b012435"
 let userPackageName = "user"
+// END KMMBRIDGE BLOCK FOR 'user'
 
 let package = Package(
-    name: "Test",
+    name: userPackageName,
     platforms: [
         .iOS(.v13)
     ],
     products: [
         .library(
-            name: corePackageName,
-            targets: [corePackageName]
-        ),
-        .library(
-            name: authPackageName,
-            targets: [authPackageName]
-        ),
-        .library(
             name: userPackageName,
             targets: [userPackageName]
-        ),
-        .library(
-            name: "Test",
-            targets: [corePackageName, authPackageName, userPackageName]
         ),
     ],
     targets: [
         .binaryTarget(
-            name: corePackageName,
-            path: "./core/build/XCFrameworks/debug/\(corePackageName).xcframework"
-        ),
-        .binaryTarget(
-            name: authPackageName,
-            path: "./auth/build/XCFrameworks/debug/\(authPackageName).xcframework",
-            dependencies: [corePackageName]
-        ),
-        .binaryTarget(
             name: userPackageName,
-            path: "./user/build/XCFrameworks/debug/\(userPackageName).xcframework",
-            dependencies: [corePackageName]
-        ),
+            url: remoteuserUrl,
+            checksum: remoteuserChecksum
+        )
+        ,
     ]
 )
