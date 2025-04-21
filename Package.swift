844cfd6a24a28e07710a4233b7cdea9126fcf5ed
Samuel Gagarin <66745577+Lavmee@users.noreply.github.com>
2025-04-21 04:43:24 +0300

    WIP

--- Package.swift
+++ Package.swift
@@ -1,9 +1,26 @@
 // swift-tools-version:5.8
 import PackageDescription
 
+// BEGIN KMMBRIDGE VARIABLES BLOCK FOR 'core' (do not edit)
+let remotecoreUrl =
+    "https://api.github.com/repos/Lavmee/KMMBridge-Test/releases/assets/248055341.zip"
+let remotecoreChecksum = "d9d0df846194d3da63e65cb7d21deca406728d6431b4dc1322f97f3d1b012435"
 let corePackageName = "core"
+// END KMMBRIDGE BLOCK FOR 'core'
+
+// BEGIN KMMBRIDGE VARIABLES BLOCK FOR 'auth' (do not edit)
+let remoteauthUrl =
+    "https://api.github.com/repos/Lavmee/KMMBridge-Test/releases/assets/248055341.zip"
+let remoteauthChecksum = "d9d0df846194d3da63e65cb7d21deca406728d6431b4dc1322f97f3d1b012435"
 let authPackageName = "auth"
+// END KMMBRIDGE BLOCK FOR 'auth'
+
+// BEGIN KMMBRIDGE VARIABLES BLOCK FOR 'user' (do not edit)
+let remoteuserUrl =
+    "https://api.github.com/repos/Lavmee/KMMBridge-Test/releases/assets/248055341.zip"
+let remoteuserChecksum = "d9d0df846194d3da63e65cb7d21deca406728d6431b4dc1322f97f3d1b012435"
 let userPackageName = "user"
+// END KMMBRIDGE BLOCK FOR 'user'
 
 let package = Package(
     name: "Test",
@@ -31,16 +48,19 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: corePackageName,
-            path: "./core/build/XCFrameworks/debug/\(corePackageName).xcframework"
+            url: remotecoreUrl,
+            checksum: remotecoreChecksum
         ),
         .binaryTarget(
             name: authPackageName,
-            path: "./auth/build/XCFrameworks/debug/\(authPackageName).xcframework",
+            url: remoteauthUrl,
+            checksum: remoteauthChecksum,
             dependencies: [corePackageName]
         ),
         .binaryTarget(
             name: userPackageName,
-            path: "./user/build/XCFrameworks/debug/\(userPackageName).xcframework",
+            url: remoteuserUrl,
+            checksum: remoteuserChecksum,
             dependencies: [corePackageName]
         ),
     ]
